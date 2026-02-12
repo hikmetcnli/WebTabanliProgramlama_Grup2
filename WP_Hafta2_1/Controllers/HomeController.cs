58a99a683410164123c7ebe2887c2b615ca487d6
hikmetcnli <hikmetcnli@hotmail.com>
2026-02-12 17:54:32 +0300

    Add project files.

---
+++ WP_Hafta2_1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using WP_Hafta2_1.Models;
+
+namespace WP_Hafta2_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult KitapKayit()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void LoginKontrol(User user)
+        {
+        
+        }
+        //metot tanımı
+        //1. erişim tipi: Public, private, protected, internal
+        //2. Hangi tipte geri değer dönecek: string, int, double, objecect, class, void
+        //3. Metodun İsmi
+
+
+
+    }
+}
